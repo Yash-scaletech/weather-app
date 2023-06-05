3278d4aa9d7e4f3de691747bb1a131ce360491c7
Yash-scaletech <yash.upadhyay@scaletech.xyz>
2023-06-05 14:20:03 +0530

    make an api call for weather data

--- src/features/home/component/homePage.tsx
+++ src/features/home/component/homePage.tsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react';
+
 import '../style/homePage.scss';
 
 const HomePage = () => {
-	return <div>HomePage</div>;
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=Y2HHEQQ9TX662V4MMAGAL9E8J')
+			.then((response) => response.json())
+			.then((json) => setData(json))
+			.catch((error) => console.error(error));
+		console.log(data);
+	}, []);
+
+	return (
+		<div>
+			<div className='wrapper position--absolute'>data</div>
+		</div>
+	);
 };
 
 export default HomePage;

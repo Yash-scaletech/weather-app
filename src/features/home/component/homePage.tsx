c3809c92d6c79c30d8d681090be2f7f3f02ac446
Yash-scaletech <yash.upadhyay@scaletech.xyz>
2023-06-06 10:42:19 +0530

    add error msg for api failure

--- src/features/home/component/homePage.tsx
+++ src/features/home/component/homePage.tsx
@@ -7,6 +7,7 @@ import '../style/homePage.scss';
 const HomePage = () => {
 	const [data, setData] = useState();
 	const [city, setCity] = useState<string>('');
+	const [errMsg, setErrMsg] = useState<boolean>(false);
 
 	useEffect(() => {
 		fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=Y2HHEQQ9TX662V4MMAGAL9E8J`)
@@ -14,7 +15,10 @@ const HomePage = () => {
 			.then((json) => {
 				setData(json);
 			})
-			.catch((error) => console.error(error));
+			.catch((error) => {
+				console.error(error);
+				setErrMsg(true);
+			});
 	}, [city]);
 
 	useEffect(() => {
@@ -22,45 +26,44 @@ const HomePage = () => {
 	}, [data]);
 
 	return (
-		<div>
-			<div className='wrapper position--absolute'>
-				<Formik
-					initialValues={{
-						cityName: ''
-					}}
-					onSubmit={(values: FormikValues) => {
-						setCity(values.cityName);
-					}}
-				>
-					{({ handleSubmit, setFieldValue, isValid }) => {
-						return (
-							<form className='flex width:full justify-content:center mt:25' onSubmit={handleSubmit}>
-								<div className='width:full'>
-									<input
-										type='text'
-										id='cname'
-										name='cityName'
-										onChange={({ target }) => {
-											setFieldValue('cityName', target.value);
-										}}
-									/>
-									<div className='mt:30 mb:30'>
-										<button
-											className={`width:full btn font-size:25 font:bold line-height:22 ${
-												isValid ? 'bg:yellow text:black' : 'bg:lighter-grey text:white no:border'
-											}`}
-											type='submit'
-											disabled={!isValid}
-										>
-											Submit
-										</button>
-									</div>
+		<div className='wrapper position--absolute'>
+			<Formik
+				initialValues={{
+					cityName: ''
+				}}
+				onSubmit={(values: FormikValues) => {
+					setCity(values.cityName);
+				}}
+			>
+				{({ handleSubmit, setFieldValue, isValid }) => {
+					return (
+						<form className='flex width:full justify-content:center mt:25' onSubmit={handleSubmit}>
+							<div className='width:full'>
+								<input
+									type='text'
+									id='cname'
+									name='cityName'
+									onChange={({ target }) => {
+										setFieldValue('cityName', target.value);
+									}}
+								/>
+								{errMsg && <div className='error'>Please Enter Valid City Name</div>}
+								<div className='mt:30 mb:30'>
+									<button
+										className={`width:full btn font-size:25 font:bold line-height:22 ${
+											isValid ? 'bg:yellow text:black' : 'bg:lighter-grey text:white no:border'
+										}`}
+										type='submit'
+										disabled={!isValid}
+									>
+										Submit
+									</button>
 								</div>
-							</form>
-						);
-					}}
-				</Formik>
-			</div>
+							</div>
+						</form>
+					);
+				}}
+			</Formik>
 		</div>
 	);
 };

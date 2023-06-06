6e413bbf2400d33f3d444f763a1a610587466195
Yash-scaletech <yash.upadhyay@scaletech.xyz>
2023-06-06 10:10:08 +0530

    use formik for city name input and got data from api for that city

--- src/features/home/component/homePage.tsx
+++ src/features/home/component/homePage.tsx
@@ -1,21 +1,66 @@
 import { useState, useEffect } from 'react';
 
+import { Formik, FormikValues } from 'formik';
+
 import '../style/homePage.scss';
 
 const HomePage = () => {
-	const [data, setData] = useState(null);
+	const [data, setData] = useState<any>(null);
+	const [city, setCity] = useState<string>('');
 
 	useEffect(() => {
-		fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=Y2HHEQQ9TX662V4MMAGAL9E8J')
+		fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=Y2HHEQQ9TX662V4MMAGAL9E8J`)
 			.then((response) => response.json())
-			.then((json) => setData(json))
+			.then((json) => {
+				setData(json);
+			})
 			.catch((error) => console.error(error));
+	}, [city]);
+
+	useEffect(() => {
 		console.log(data);
-	}, []);
+	}, [data]);
 
 	return (
 		<div>
-			<div className='wrapper position--absolute'>data</div>
+			<div className='wrapper position--absolute'>
+				<Formik
+					initialValues={{
+						cityName: ''
+					}}
+					onSubmit={(values: FormikValues) => {
+						setCity(values.cityName);
+					}}
+				>
+					{({ handleSubmit, setFieldValue, isValid }) => {
+						return (
+							<form className='flex width:full justify-content:center mt:25' onSubmit={handleSubmit}>
+								<div className='width:full'>
+									<input
+										type='text'
+										id='cname'
+										name='cityName'
+										onChange={({ target }) => {
+											setFieldValue('cityName', target.value);
+										}}
+									/>
+									<div className='mt:30 mb:30'>
+										<button
+											className={`width:full btn font-size:25 font:bold line-height:22 ${
+												isValid ? 'bg:yellow text:black' : 'bg:lighter-grey text:white no:border'
+											}`}
+											type='submit'
+											disabled={!isValid}
+										>
+											Submit
+										</button>
+									</div>
+								</div>
+							</form>
+						);
+					}}
+				</Formik>
+			</div>
 		</div>
 	);
 };
